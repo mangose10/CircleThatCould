222406a8870521391fe6030832410cf69546060e
mangose10 <leonar98@knights.ucf.edu>
2016-11-19 03:02:54 -0500

    start-up

---
+++ Assets/Scripts/EnemyTAI.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTAI : MonoBehaviour {
+
+    private Rigidbody2D tRigidBody;
+    private Animator tAnimator;
+    [SerializeField]
+    private float speed = 1;
+    [SerializeField]
+    private bool isGroundedL;
+    [SerializeField]
+    private bool isGroundedR;
+    [SerializeField]
+    private bool isWalled;
+    [SerializeField]
+    private float colRadius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private Transform[] groundRightCheck;
+    [SerializeField]
+    private Transform[] groundLeftCheck;
+    [SerializeField]
+    private Transform[] wallCheck;
+    private bool timer = true;
+    private float timed;
+    private bool facingRight = true;
+    [SerializeField]
+    private GameObject player;
+    private bool defenseFlag = false;
+
+    // Use this for initialization
+    void Start () {
+        tRigidBody = GetComponent<Rigidbody2D>();
+        tAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        isGroundedL = CheckLeftGround();
+        isGroundedR = CheckRightGround();
+        isWalled = CheckWall();
+        timed += Time.deltaTime;
+        timer = timerF(timer, timed);
+        Flip(speed);
+        tAnimator.SetFloat("distance", Mathf.Abs(player.transform.position.x - transform.position.x));
+        tAnimator.SetBool("facing", facingPlayer());
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) < 5 && facingPlayer())
+        {
+            defenseFlag = true;
+        }
+        Movement(isGroundedL, isGroundedR, isWalled);
+	}
+    bool facingPlayer(){
+        if ((player.transform.position.y - transform.position.y < 5) && (player.transform.position.y - transform.position.y > -0.5)) {
+            if (((player.transform.position.x > transform.position.x) && (speed > 0)) || ((player.transform.position.x < transform.position.x) && (speed < 0)))
+                return true; 
+            else
+                return false;
+        }
+        else
+            return false;
+            
+    }
+    void Movement(bool isGroundedL, bool isGroundedR, bool isWalled){
+        if (!defenseFlag)
+            tRigidBody.velocity = new Vector2(speed, tRigidBody.velocity.y);
+        Debug.Log(tRigidBody.velocity.x);
+        if ((!isGroundedR|| !isGroundedL || isWalled) && timer && !defenseFlag) {
+            speed *= -1;
+            Debug.Log("hit");
+            timer = false;
+            timed = 0;
+        }
+        
+    }
+    bool timerF(bool timer, float timed){
+        if (timed >= 1) { return true; }
+        else { return false; }
+    }
+
+    private bool CheckRightGround(){
+        foreach (Transform point in groundRightCheck)
+        {
+            Collider2D[] grColliders = Physics2D.OverlapCircleAll(point.position, colRadius, whatIsGround);
+
+            for (int i = 0; i < grColliders.Length; i++)
+            {
+                if (grColliders[i].gameObject != gameObject)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private bool CheckLeftGround()
+    {
+        foreach (Transform point in groundLeftCheck)
+        {
+            Collider2D[] glColliders = Physics2D.OverlapCircleAll(point.position, colRadius, whatIsGround);
+
+            for (int i = 0; i < glColliders.Length; i++)
+            {
+                if (glColliders[i].gameObject != gameObject)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private bool CheckWall()
+    {
+        foreach (Transform point in wallCheck)
+        {
+            Collider2D[] wColliders = Physics2D.OverlapCircleAll(point.position, colRadius, whatIsGround);
+
+            for (int i = 0; i < wColliders.Length; i++)
+            {
+                if (wColliders[i].gameObject != gameObject)
+                {
+                    Debug.Log(wColliders[i]);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private void Flip(float horizontal)
+    {
+        if (((horizontal > 0) && !facingRight) || ((horizontal < 0) && facingRight))
+        {
+
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
---
+++ Assets/Scripts/playerMovementR.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+public class playerMovementR : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+    [SerializeField]
+    float speed = 4;
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float groundRadius;
+    private bool facingRight = true;
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private bool isGrounded = true;
+    private bool jump;
+    [SerializeField]
+    private float jumpSpeed;
+
+    Collider2D StandingCollider;
+    Collider2D RollingCollider;
+
+    // Use this for initialization
+    void Start () {
+        myRigidBody = GetComponent <Rigidbody2D> ();
+        myAnimator = GetComponent<Animator>();
+        StandingCollider = GetComponent<PolygonCollider2D>();
+        RollingCollider = GetComponent<CircleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        isGrounded = IsGrounded();
+        Movement(horizontal, vertical);
+        Flip(horizontal);
+        jump = false;
+        ChangeCollider(horizontal);
+        
+    }
+
+    void ChangeCollider(float horizontal)
+    {
+        if (Mathf.Abs(horizontal) > 0.2)
+        {
+            StandingCollider.enabled = false;
+            RollingCollider.enabled = true;
+        }
+        else
+        {
+            StandingCollider.enabled = (true);
+            RollingCollider.enabled = (false);
+        }
+    }
+
+    void Movement(float horizontal, float vertical) {
+
+        myRigidBody.velocity = new Vector2(horizontal*speed, myRigidBody.velocity.y);
+        
+        if (isGrounded && (vertical > 0.2)){
+            jump = true;
+        }
+        if (isGrounded && jump)
+        {
+            myRigidBody.AddForce(new Vector2(0, jumpSpeed));
+            isGrounded = false;
+        }
+        myAnimator.SetFloat("move", Mathf.Abs(horizontal));
+    }
+    private void Flip(float horizontal){
+        if (((horizontal > 0) && !facingRight) || ((horizontal < 0) && facingRight)){
+
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+    private bool IsGrounded() {
+        if (myRigidBody.velocity.y <= 0){
+            foreach(Transform point in groundPoints){
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++){
+                    if (colliders[i].gameObject != gameObject){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
